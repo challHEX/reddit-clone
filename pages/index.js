6f1d6465e84335b3c4e51d4ab80308499c27c45d
Christian <challhex@duck.com>
2022-07-21 00:49:33 +0200

    Add data to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
-	return 'Hello';
+import prisma from 'lib/prisma';
+import Head from 'next/head';
+import Posts from 'components/Posts';
+import { getPosts } from 'lib/data';
+
+export default function Home({ posts }) {
+	return (
+		<div>
+			<Head>
+				<title>Reddit Clone</title>
+				<meta name='description' content='A great social network!' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Posts posts={posts} />
+		</div>
+	);
+}
+
+export async function getServerSideProps() {
+	let posts = await getPosts(prisma);
+	posts = JSON.parse(JSON.stringify(posts));
+
+	return {
+		props: {
+			posts
+		}
+	};
 }
